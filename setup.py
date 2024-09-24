3f08bfbdaa3d1702e1bf6bc32ad2b029de199f3b
froggagul <froggagul@gmail.com>
2024-09-24 16:10:03 +0900

    feat: change build name

--- setup.py
+++ setup.py
@@ -2,7 +2,11 @@ from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
 
-setup(ext_modules=[Extension("ikfastpy", 
-                            ["ikfastpy.pyx", 
-                             "ikfast_wrapper.cpp"], language="c++", libraries=['lapack'])],
-      cmdclass = {'build_ext': build_ext})
+setup(
+    name='ikfastpy',
+    version='0.0.1',
+    ext_modules=[
+        Extension("ikfastpy", ["ikfastpy.pyx", "ikfast_wrapper.cpp"], language="c++", libraries=['lapack'])
+    ],
+    cmdclass = {'build_ext': build_ext}
+)
